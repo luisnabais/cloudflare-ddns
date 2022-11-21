f64b1833ff005ba43247ee7ea410e1d4e11a8052
Luís Nabais <mail@nabais.eu>
2022-11-21 16:55:08 +0000

    Don't connect to CF to update IP if hasn't changed

--- app/update_ddns.py
+++ app/update_ddns.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 IP_API = 'https://api.ipify.org?format=json'
 USE_PUBLIC_IP=True
+CURRENT_IP_FILE="current_ip.txt"
 
 
 def print_ts(message):
@@ -27,6 +28,19 @@ def get_public_ip():
     print_ts('Public IP address ' + resp.json()['ip'])
     return resp.json()['ip']
 
+def get_old_ip(ip_addr):
+    if not os.path.exists(CURRENT_IP_FILE):
+        open(CURRENT_IP_FILE, "w").close
+        return None
+    
+    with open(CURRENT_IP_FILE, "r") as file:
+        content = file.read()
+
+        if content == "":
+            return None
+        else:
+            return content
+
 def get_records():
     resp = requests.get(
         'https://api.cloudflare.com/client/v4/zones/{}/dns_records'.format(os.environ['ZONE_ID']),
@@ -37,6 +51,15 @@ def get_records():
     print_ts(json.dumps(resp.json(), indent=4, sort_keys=True))
     print_ts('Please find the DNS record ID you would like to update and entry the value into the script')
     sys.exit(0)
+        
+def save_current_ip(ip_addr):
+    with open(CURRENT_IP_FILE, "w") as file:
+        file.write(ip_addr)
+
+def wait_ttl():
+    print_ts('Waiting ' + os.environ['TTL'] + ' seconds...')
+    time.sleep(int(os.environ['TTL']))
+    print()
 
 
 ########
@@ -48,6 +71,16 @@ while True:
         ip_addr = get_public_ip()
     else:
         ip_addr = os.environ['IP_ADDRESS']
+
+    old_ip = get_old_ip(ip_addr)
+    
+    if old_ip == ip_addr:
+        print_ts("Current IP (" + ip_addr + ") hasn't changed (" + old_ip + ").")
+        wait_ttl()
+        continue
+
+    if old_ip is None:
+        print_ts("Can't get old IP information.")
 
     if 'RECORD_ID' not in os.environ:
         get_records()
@@ -68,8 +101,6 @@ while True:
     assert resp.status_code == 200
 
     print_ts('Updated DNS record for {}'.format(ip_addr, os.environ['HOSTNAME']))
+    save_current_ip(ip_addr)    
 
-
-    print_ts('Waiting ' + os.environ['TTL'] + ' seconds...')
-    time.sleep(int(os.environ['TTL']))
-    print()
+    wait_ttl()
